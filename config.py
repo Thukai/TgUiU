aeb32a8f772f413380321396a9efc1c42988bf09
Thukai <hukaitaniya@gmail.com>
2025-02-26 06:07:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config(object):
   
   API_HASH = os.getenv("apihash","89074aa74ed297150b3120914946db5c")
   
-  BOT_TOKEN = os.getenv("token","7853381482:AAHJlEEAj3k-Ysif9gKZwSEOe0oBq2wsgtI")
+  BOT_TOKEN = os.getenv("token","7586819606:AAGW_cM_HcbcRlk7AmnAvQ8xXOMaa1aKwZE")
   
   AUTH = os.getenv("auth","1387186514")
   
