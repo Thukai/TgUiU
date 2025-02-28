0f5e007de1cd29e25d0b10cebcf71a09cd35e597
Thukai <hukaitaniya@gmail.com>
2025-02-28 11:32:59 +0530

    Update ext.py

--- sites/ext.py
+++ sites/ext.py
@@ -5,6 +5,7 @@ import urllib.parse
 # Dictionary to map domains to extractor scripts
 EXTRACTORS = {
     'spankbang.com': 'sites.spbank',
+    'eporner': 'sites.epr.py',
 }
 
 def get_domain(url):
@@ -12,12 +13,19 @@ def get_domain(url):
     parsed_url = urllib.parse.urlparse(url)
     return parsed_url.netloc.lower()
 
+def find_s(domain):
+    for k in EXTRACTORS:
+        if k in domain:
+            return k
+    return None
+
+    
 def run_extractor(url):
     """Run the corresponding extractor script based on the domain."""
     domain = get_domain(url)
-
-    if domain in EXTRACTORS:
-        script_name = EXTRACTORS[domain]
+    key = find_s(domain)
+    if key:
+        script_name = EXTRACTORS[key]
         # Dynamically import the module based on the domain
         extractor = importlib.import_module(script_name)
         # Assuming each script has an `extract_data(url)` function to extract data
