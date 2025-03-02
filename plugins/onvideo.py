42776a5ff0db7d6193b51c1928aeb60e12f22bb3
Thukai <hukaitaniya@gmail.com>
2025-03-02 14:43:07 +0530

    Create onvideo.py

---
+++ plugins/onvideo.py
@@ -0,0 +1,29 @@
+import os, asyncio
+import time
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery, InputMediaPhoto
+from plugins.authers import is_authorized
+from tgdw import download_file
+from git_up import to_git
+
+download_dir = "/forGit"
+if not os.path.exists(download_dir):
+    os.makedirs(download_dir)
+
+@Client.on_message(filters.command("git"))
+async def up_to_git(client, message):
+  if not message.reply_to_message or not message.reply_to_message.video:
+    await message.reply("Please reply to video message with this command")
+    return
+  if not is_authorized(message.chat.id):
+    await message.reply("**❌️You are not authorized to use me!❌️**")
+    return
+  v_msg = message.reply_to_message
+  msg = await message.reply("Trying to start!...")
+  file_path = await download_file(client, v_msg, download_dir, msg)
+  if file_path:
+    r = await to_git(file_path, msg)
+    
+
+  
+    
