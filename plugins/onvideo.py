94b57bed9c6605d599a0111e0fd5cb1c92366da1
Thukai <hukaitaniya@gmail.com>
2025-03-03 10:36:44 +0530

    Update onvideo.py

--- plugins/onvideo.py
+++ plugins/onvideo.py
@@ -12,19 +12,35 @@ if not os.path.exists(download_dir):
 
 @Client.on_message(filters.command("git"))
 async def up_to_git(client, message):
-  if not message.reply_to_message or not message.reply_to_message.video:
-    await message.reply("Please reply to video message with this command")
-    return
-  if not is_authorized(message.chat.id):
-    await message.reply("**❌️You are not authorized to use me!❌️**")
-    return
-  v_msg = message.reply_to_message
-  v_path=  os.path.join(download_dir,v_msg.video.file_name)
-  msg = await message.reply(f"Trying to start!...\n {v_path}")
-  file_path = await download_file(client, v_msg, download_dir, msg)
-  if file_path:
-    r = await to_git(file_path, msg)
+    if not message.reply_to_message or not message.reply_to_message.video:
+        await message.reply("Please reply to video message with this command")
+        return
     
-
-  
+    if not is_authorized(message.chat.id):
+        await message.reply("**❌️You are not authorized to use me!❌️**")
+        return
     
+    v_msg = message.reply_to_message
+    v_path = os.path.join(download_dir, v_msg.video.file_name)
+    msg = await message.reply(f"Trying to start!...\n {v_path}")
+    
+    # Parse additional arguments (threads, extra arguments)
+    command_args = message.text.split(maxsplit=1)
+    
+    threads = None
+    extra_args = []
+    
+    if len(command_args) > 1:
+        params = command_args[1].split()
+        for param in params:
+            if param.startswith("-threads"):
+                # Extract the number of threads after '-threads'
+                threads = int(param.split("=")[1]) if "=" in param else 4  # default to 4 if not specified
+            else:
+                extra_args.append(param)
+    
+    # Download the video file
+    file_path = await download_file(client, v_msg, download_dir, msg)
+    if file_path:
+        # Pass threads and extra_args to the to_git function
+        r = await to_git(file_path, msg, trs=threads, extra=extra_args)
